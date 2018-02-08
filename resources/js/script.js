73673abd12b63d42b4e5721b62bef1f8f6ca22ff
Prajakta Gharat <prajaktagharat28@gmail.com>
2018-02-08 14:45:57 -0500

    First Commit

---
+++ resources/js/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+        
+    /* Mobile navigation */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round'); 
+        }        
+    });
+    
+    
+    
+    
+    
+    
+    
+    $(window).resize(function(){
+
+var nav = $('.js--main-nav');
+
+var icon = $('.js--nav-icon i');
+
+if ($(window).width() > 767){
+
+nav.css("display", "block");
+
+icon.addClass('ion-close-round');
+
+icon.removeClass('ion-navicon-round');
+
+} else {
+
+nav.css("display", "none");
+
+icon.addClass('ion-navicon-round');
+
+icon.removeClass('ion-close-round');
+
+}
+});
+});
